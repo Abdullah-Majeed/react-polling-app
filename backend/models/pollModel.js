e786a8853a30d0fbb0f34ab0e321ddaf53424d49
Abdullah Majeed <abdullahmajeed969@gmail.com>
2024-11-21 02:03:26 +0500

    authentication api integrated

--- backend/models/pollModel.js
+++ backend/models/pollModel.js
@@ -19,7 +19,11 @@ const pollSchema = new Schema({
     image: {
         type: String,
         required: true,
-    }
+    },
+    // user_id: {
+    //     type: String,
+    //     required: true,
+    // }
 }, { timestamps: true });
 
 module.exports = mongoose.model('Poll', pollSchema);
--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,15 @@ require('dotenv').config()
 const express = require('express');
 const mongoose = require('mongoose');
 const pollRoutes = require("./router/polls")
+const userRoutes = require("./router/user")
 const app = express();
 
 app.use(express.json());
 app.use((req, res, next) => {
     next();
 });
-app.use('/api/polls/', pollRoutes);
+app.use('/api/polls', pollRoutes);
+app.use('/api/user', userRoutes);
 mongoose.connect(process.env.MONGO_URI).then(() => {
     app.listen(process.env.PORT_NO, () => {
         console.log(`Backend service listening on ${process.env.PORT_NO}`)
